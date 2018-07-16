e2ba31d51d3a218969263d66eb6458dd7a80353a
Amir Rustamzadeh <amir@cypress.io>
2018-07-16 13:59:42 -0700

    feat: add recursive trim
    - add reduceToSchema function to recursively handle accumulation of schema valid props
    - person@1.2.0 schema to test recursive trim
    - updated document test snapshots
    - Related #123

--- test/example-schemas.ts
+++ test/example-schemas.ts
@@ -8,6 +8,34 @@ import {
 import { ObjectSchema, SchemaCollection, VersionedSchema } from '../src/objects'
 import { combineSchemas, versionSchemas } from '../src/utils'
 
+const traits100: ObjectSchema = {
+  version: {
+    major: 1,
+    minor: 0,
+    patch: 0,
+  },
+  schema: {
+    type: 'object',
+    title: 'Traits',
+    description: 'Physical traits of person',
+    properties: {
+      eyeColor: {
+        type: 'string',
+        description: 'Eye color',
+      },
+      hairColor: {
+        type: 'string',
+        description: 'Hair color',
+      },
+    },
+    additionalProperties: false,
+  },
+  example: {
+    eyeColor: 'brown',
+    hairColor: 'black',
+  },
+}
+
 const name: CustomFormat = {
   name: 'name',
   description: 'Custom name format',
@@ -64,6 +92,26 @@ const person110: ObjectSchema = extend(person100, {
   },
   example: {
     title: 'mr',
+  },
+})
+
+const person120: ObjectSchema = extend(person110, {
+  schema: {
+    description: 'Person with traits',
+    properties: {
+      traits: {
+        ...traits100.schema,
+        see: traits100,
+      },
+    },
+  },
+  example: {
+    name: 'Joe',
+    age: 10,
+    traits: {
+      eyeColor: 'brown',
+      hairColor: 'black',
+    },
   },
 })
 
@@ -134,7 +182,11 @@ const car110: ObjectSchema = extend(car100, {
 // collection of "Person" schemas by version.
 // In our case there will be single version, but here is where we can combine multiple
 // versions like: versionSchemas(person100, person110, person200, ...)
-const personVersions: VersionedSchema = versionSchemas(person100, person110)
+const personVersions: VersionedSchema = versionSchemas(
+  person100,
+  person110,
+  person120,
+)
 const teamVersions: VersionedSchema = versionSchemas(team100)
 const carVersions: VersionedSchema = versionSchemas(car100, car110)
 
@@ -147,4 +199,12 @@ const schemas: SchemaCollection = combineSchemas(
 
 const formats: JsonSchemaFormats = detectors(exampleFormats)
 
-export { car100, person100, person110, formats, schemas, exampleFormats }
+export {
+  car100,
+  person100,
+  person110,
+  person120,
+  formats,
+  schemas,
+  exampleFormats,
+}
